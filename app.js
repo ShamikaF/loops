c754b75f083df80a2cebbee35859ebe82498c29f
LCAdmin3 <lcadmin3@LCAdmin3s-MacBook-Air.local>
2016-05-11 09:03:50 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+var b = "Blackpower";
+var a = "Amen";
+var results = a + " " +b;
+console.log(results);
---
+++ apps.js
@@ -0,0 +1,14 @@
+if ( 20 x 4 === 75) {
+	console.log("99 bottles rootbeer on the wall");
+} else {
+	console.log("99 bottles rootbeer on the wall");
+
+	var x = rootbeer;
+	var y = 99;
+	var results = y + x;
+	console.log("x+y")
+
+	console.log (rootbeer.length);
+	for(var i=6; i >= rootbeer.length -9; i ++) {
+	console.log(i + " " + rootbeer[i]);
+	}
